65458e55758dd548df3516f51e989c9e027826d9
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-31 19:33:44 +0100

    clone project, and init repo

---
+++ src/main/java/com/example/demoCacheApp/DemoCacheAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCacheApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCacheAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCacheAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoCacheApp/DemoCacheAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCacheApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCacheAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
