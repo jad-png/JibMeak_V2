a37d83fe12c441e4d0d62703c68f870ee30a5b06
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-05 00:59:51 +0100

    feat(controller): reveal Customer management endpoint via creation of CustomerController

---
+++ src/main/java/com/taxist/JibMeakV2/controller/CustomerController.java
@@ -0,0 +1,45 @@
+package com.taxist.JibMeakV2.controller;
+
+import com.taxist.JibMeakV2.dto.CustomerDTO;
+import com.taxist.JibMeakV2.model.Customer;
+import com.taxist.JibMeakV2.service.interfaces.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+    private final CustomerService service;
+
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
+        List<CustomerDTO> customers = service.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping
+    public ResponseEntity<Optional<CustomerDTO>> getCustomerById(@RequestParam Long id) {
+        Optional<CustomerDTO> customerDTO = service.getCustomerById(id);
+        return ResponseEntity.ok(customerDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDTO> saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        CustomerDTO saved = service.createCustomer(customerDTO);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCustomer(@RequestParam Long id) {
+        service.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+}
