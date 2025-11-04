1b4ac4be50df323a01bb78b9e0b8e32a6fda479c
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-04 19:58:52 +0100

    feat(model): create and encapsulte delivery history class

---
+++ src/main/java/com/taxist/JibMeakV2/model/DeliveryHistory.java
@@ -0,0 +1,57 @@
+package com.taxist.JibMeakV2.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "delivery_history")
+public class DeliveryHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id",  nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_id", nullable = false)
+    private Tour tour;
+
+    @Column(name = "delivery_date")
+    private LocalDate deliveryDate;
+
+    @Column(name = "planned_time")
+    private LocalTime plannedTime;
+
+    @Column(name = "actual_time")
+    private LocalTime actualTime;
+
+    @Column(name = "delay_in_minutes")
+    private Long delayInMinutes;
+
+    @Column(name = "day_of_week")
+    private String dayOfWeek;
+
+    public Long getCustomerId() { return customer.getId(); }
+    public void setCustomer(Customer customer) { this.customer = customer; }
+
+    public Long getTourId() { return tour.getId(); }
+    public void setTourId(Tour tour) { this.tour = tour; }
+
+    public LocalDate getDeliveryDate() { return deliveryDate; }
+    public void setDeliveryDate(LocalDate deliveryDate) { this.deliveryDate = deliveryDate; }
+
+    public LocalTime getPlannedTime() { return plannedTime; }
+    public LocalTime setPlannedTime(LocalTime plannedTime) {  this.plannedTime = plannedTime; return this.plannedTime; }
+
+    public LocalTime getActualTime() { return actualTime; }
+    public LocalTime setActualTime(LocalTime actualTime) { this.actualTime = actualTime; return this.actualTime; }
+
+    public Long getDelayInMinutes() { return delayInMinutes; }
+
+    public String getDayOfWeek() { return dayOfWeek; }
+    public void setDayOfWeek(String dayOfWeek) { this.dayOfWeek = dayOfWeek; }
+}
