7a6eade88a5da3a10bedb4f619cf0931b3057f15
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-05 01:14:41 +0100

    refac(service): refactore create delivery business logic, based on costumer

--- src/main/java/com/taxist/JibMeakV2/service/Impl/DeliveryServiceImpl.java
+++ src/main/java/com/taxist/JibMeakV2/service/Impl/DeliveryServiceImpl.java
@@ -3,7 +3,9 @@ package com.taxist.JibMeakV2.service.Impl;
 
 import com.taxist.JibMeakV2.dto.DeliveryDTO;
 import com.taxist.JibMeakV2.mapper.DeliveryMapper;
+import com.taxist.JibMeakV2.model.Customer;
 import com.taxist.JibMeakV2.model.Delivery;
+import com.taxist.JibMeakV2.repository.CustomerRepository;
 import com.taxist.JibMeakV2.repository.DeliveryRepository;
 import com.taxist.JibMeakV2.service.interfaces.DeliveryService;
 import com.taxist.JibMeakV2.utils.DeliveryValidationUtils;
@@ -17,16 +19,25 @@ import java.util.stream.Collectors;
 public class DeliveryServiceImpl implements DeliveryService {
     private final DeliveryRepository deliveryRepo;
     private final DeliveryMapper mapper;
+    private final CustomerRepository costumerRepo;
 
-    public DeliveryServiceImpl(DeliveryRepository deliveryRepo, DeliveryMapper mapper) {
+    public DeliveryServiceImpl(DeliveryRepository deliveryRepo,
+                               DeliveryMapper mapper,
+                               CustomerRepository costumerRepo) {
         this.deliveryRepo = deliveryRepo;
         this.mapper = mapper;
+        this.costumerRepo = costumerRepo;
     }
 
     public DeliveryDTO createDelivery(DeliveryDTO dto) {
         Delivery delivery = mapper.toEntity(dto);
+        Customer customer = costumerRepo.findById(dto.getCostumerId()).orElse(null);
+
+        delivery.setCustomer(customer);
+
         DeliveryValidationUtils.validateDelivery(delivery);
         Delivery savedDelivery = deliveryRepo.save(delivery);
+
         return mapper.toDTO(savedDelivery);
     }
 
