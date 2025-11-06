64e9e69511ba9a507c3a0bb8ebf4106301a6aa77
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-06 21:24:25 +0100

    feat(repo): add method to find tour by id with it's deliveries, using natural @query annotation

--- src/main/java/com/taxist/JibMeakV2/repository/TourRepository.java
+++ src/main/java/com/taxist/JibMeakV2/repository/TourRepository.java
@@ -4,14 +4,20 @@ import com.taxist.JibMeakV2.model.Delivery;
 import com.taxist.JibMeakV2.model.Tour;
 import com.taxist.JibMeakV2.model.enums.TourStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface TourRepository extends JpaRepository<Tour, Long> {
     List<Tour> findByDate(LocalDate date);
 
     List<Tour> findByStatus(TourStatus status);
+
+    @Query("SELECT t FROM Tour t JOIN FETCH t.deliveries WHERE t.id = :id")
+    Optional<Tour> findByIdWithDeliveries(@Param("id") Long id);
 
 //    List<Tour> findByTourVehicleIdAndDate(Long vehicleId, LocalDate date);
 //
