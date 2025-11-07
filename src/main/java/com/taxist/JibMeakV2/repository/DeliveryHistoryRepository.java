5d406bbb964fffb5db8b278e957e4e399a04cc59
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-07 01:09:55 +0100

    feat(repo): define method that get deliveries made by tour

--- src/main/java/com/taxist/JibMeakV2/repository/DeliveryHistoryRepository.java
+++ src/main/java/com/taxist/JibMeakV2/repository/DeliveryHistoryRepository.java
@@ -2,6 +2,13 @@ package com.taxist.JibMeakV2.repository;
 
 import com.taxist.JibMeakV2.model.DeliveryHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface DeliveryHistoryRepository extends JpaRepository<DeliveryHistory, Long> {
+    @Query("SELECT dh FROM DeliveryHistory dh JOIN FETCH dh.tour WHERE dh.tour.id = :tourId")
+    List<DeliveryHistory> findByTourId(@Param("tourId") Long tourId);
 }
