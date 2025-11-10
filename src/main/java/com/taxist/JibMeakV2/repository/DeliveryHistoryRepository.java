000fe6fc0687e8d6630ed18d96da63e780d97f31
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-10 19:05:27 +0100

    feat(repo): define method that find deliveries history that a costumer is IN

--- src/main/java/com/taxist/JibMeakV2/repository/DeliveryHistoryRepository.java
+++ src/main/java/com/taxist/JibMeakV2/repository/DeliveryHistoryRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface DeliveryHistoryRepository extends JpaRepository<DeliveryHistory, Long> {
     @Query("SELECT dh FROM DeliveryHistory dh JOIN FETCH dh.tour WHERE dh.tour.id = :tourId")
     List<DeliveryHistory> findByTourId(@Param("tourId") Long tourId);
+
+    // better approach for getting history where costumer is included, to n+1
+    @Query("Select dh from DeliveryHistory dh " +
+            "JOIN FETCH dh.customer c " +
+            "JOIN FETCH dh.tour t " +
+            "WHERE c.id IN :costumerIds")
+    List<DeliveryHistory> findByCostumerIdIn(@Param("costumerIds") List<Long> costumerIds);
 }
